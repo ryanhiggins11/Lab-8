fcd671e864192cdbaf22b8a0c9fb789f515d0efc
Martin Kenirons <martin.kenirons@gmit.ie>
2019-10-24 14:34:43 +0100

    lab uploaded

--- src/components/create.js
+++ src/components/create.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class Create extends React.Component {
   constructor(props){
@@ -30,6 +31,18 @@ class Create extends React.Component {
     alert(this.state.Title+ "      " + this.state.Year 
     +"       "+ this.state.Poster);
     e.preventDefault();
+
+    const movieObject = {
+      title:this.state.Title,
+      year:this.state.Year,
+      poster:this.state.Poster
+    }
+
+    axios.post('http://localhost:4000/api/movies',movieObject)
+    .then()
+    .catch();
+
+
     this.setState({Title:'',
                   Year:'',
                 Poster:''});
